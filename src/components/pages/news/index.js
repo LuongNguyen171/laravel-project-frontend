ecdc13e5849567c94df8354e0bf3bf5b791e5ed8
Lưu Tinh Anh <84430093+tinhanhluu@users.noreply.github.com>
2023-12-06 21:03:03 +0700

    Update index.js

--- src/components/pages/news/index.js
+++ src/components/pages/news/index.js
@@ -2,12 +2,68 @@ import classNames from 'classnames/bind';
 import styles from './News.module.scss';
 
 const cx = classNames.bind(styles);
+
 function News() {
-    return (<div className={cx('wrapper')}>
-        <h1>
-            Chức năng chưa được thiết lập
-        </h1>
-    </div>);
+    const [blogs, setBlogs] = useState(blogList);
+
+    // lOGIC FOR SEARCHBAR
+    // const [searchKey, setSearchKey] = useState('');
+
+    // //Search Submit
+    // const handleSearchSubmit = (event) => {
+    //     event.preventDefault();
+    //     handleSearchResults();
+    // };
+
+    // //Search for blogs by category
+    // const handleSearchResults = () => {
+    //     const allBlogs = blogList;
+    //     const filteredBlogs = allBlogs.filter((blog) =>
+    //         blog.category
+    //             .toLowerCase()
+    //             .includes(searchKey.toLowerCase().trim()),
+    //     );
+
+    //     setBlogs(filteredBlogs);
+    // };
+
+    // const handleClearSearch = () => {
+    //     setBlogs(blogList);
+    //     setSearchKey('');
+    // };
+
+    // LOGIC FOR STICKY SIDEBAR
+    // const [fix, setFix] = useState(false);
+    // function setFixedSidebar() {
+    //     if (window.scrollY >= 500) {
+    //         setFix(true);
+    //     } else {
+    //         setFix(false);
+    //     }
+    // }
+    // window.addEventListener('scroll', setFixedSidebar);
+    
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('container')}>
+                <div className={cx('left-content')}>
+                    <SideBar blogs={blogs} />
+                </div>
+                <div className={cx('right-content')}>
+                    <a href="#">
+                        <h1>TIN TỨC</h1>
+                    </a>
+                    <div className={cx('main-content')}>
+                        {!blogs.length ? (
+                            <EmptyList />
+                        ) : (
+                            <BlogList blogs={blogs} />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default News;
